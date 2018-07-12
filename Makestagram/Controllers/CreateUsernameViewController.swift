f39eb8e328a01919fb89388b5d3c51a79008f3bc
theconnieliu <connieliu73@gmail.com>
2018-07-12 10:53:49 -0700

    Built Create Username page and enabled addition of users into database

---
+++ Makestagram/Controllers/CreateUsernameViewController.swift
@@ -0,0 +1,57 @@
+//
+//  CreateUsernameViewController.swift
+//  Makestagram
+//
+//  Created by Connie Liu on 7/12/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+
+class CreateUsernameViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        guard let firUser = Auth.auth().currentUser,
+           let username = usernameTextField.text,
+            !username.isEmpty else {return}
+        let userAttrs = ["username": username]
+        let ref = Database.database().reference().child("users").child(firUser.uid)
+        ref.setValue(userAttrs) { (error, ref) in
+            if let error = error {
+                assertionFailure(error.localizedDescription)
+                return
+            }
+            ref.observeSingleEvent(of: .value, with: {(snapshot) in
+                let user = User(snapshot: snapshot)
+            })
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Makestagram/Controllers/LoginViewController.swift
+++ Makestagram/Controllers/LoginViewController.swift
@@ -7,14 +7,23 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseUI
+import FirebaseDatabase
+
+typealias FIRUser = FirebaseAuth.User
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var loginButton: UIButton!
     @IBAction func loginButtonClicked(_ sender: UIButton) {
-        print("login button clicked")
+        guard let authUI = FUIAuth.defaultAuthUI()
+            else {return}
+        authUI.delegate = self
+        let authViewController = authUI.authViewController()
+        present(authViewController, animated: true)
+        //print("login button clicked")
     }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,4 +47,37 @@ class LoginViewController: UIViewController {
     }
     */
 
+}
+extension LoginViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith user: FIRUser?, error: Error?) {
+        if let error = error {
+            assertionFailure("Error signing in: \(error.localizedDescription)")
+            return
+        }
+        print("handle user signup / login")
+    }
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?){
+        if let error = error {
+            assertionFailure("Error signing in: \(error.localizedDescription)")
+            return
+        }
+        guard let user = authDataResult?.user
+            else {return}
+        let userRef = Database.database().reference().child("users").child(user.uid)
+        
+//        userRef.observeSingleEvent(of: .value, with: { (snapshot) in
+//            if let user = User(snapshot: snapshot) {
+//                print("Welcome back, \(user.username).")
+//            } else {
+//                print("New user!")
+//            }
+//        })
+        userRef.observeSingleEvent(of: .value, with: { [unowned self] (snapshot) in
+            if let user = User(snapshot: snapshot) {
+                print("Welcome back, \(user.username).")
+            } else {
+                self.performSegue(withIdentifier: "toCreateUsername", sender: self)
+            }
+        })
+    }
 }
